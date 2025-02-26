05a070d5b2924849d8a62757a5ce5a3ab91bba1d
Yjin <Yjin@DESKTOP-PNAQRJU>
2025-02-26 20:41:27 +0900

    lv0_코드테스트

---
+++ src/lv0/두수의곱.java
@@ -0,0 +1,20 @@
+package lv0;
+
+public class 두수의곱 {
+
+    /*
+     * 정수 num1, num2가 매개변수 주어집니다. 
+     * num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+     */
+    
+    public int solution(int num1, int num2) {
+            int answer = 0;
+            
+            return answer= num1 * num2;
+    }
+      
+        
+
+}
+
+
