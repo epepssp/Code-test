22558d1d3b32450e81c6e5d7de06ec90b582fc92
epepssp <101303ee@naver.com>
2025-03-04 13:25:47 +0900

    lv1_코드테스트 6일차

---
+++ src/lv1/평균구하기.java
@@ -0,0 +1,17 @@
+package lv1;
+
+public class 평균구하기 {
+
+    /*
+     * 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+     */
+    
+    public double solution(int[] arr) {
+        int answer = 0;
+        
+        for (int num : arr) {
+            answer += num;
+        }
+        return (double) answer / arr.length;
+    }
+}
